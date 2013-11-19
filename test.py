eec70ad226571958915d0ff510bd6b252ac6c5cb
graczyk <nick.graczyk@gmail.com>
2013-11-19 08:51:28 -0500

    oauth now kinda works, need to make url open in browser

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import pytumblr
 import json
 import random
+import oauth2 as oauth
+import urlparse
 from flask import *
 
+request_token_url = 'http://www.tumblr.com/oauth/request_token'
+authorize_url = 'http://www.tumblr.com/oauth/authorize'
+access_token_url = 'http://www.tumblr.com/oauth/access_token'
+
 app = Flask(__name__)
+setupdone = False
 
 @app.route('/top')
 def total():
@@ -23,29 +30,61 @@ def update():
     
 @app.route('/')
 def mash():
+    if not setupdone:
+        setup()
     pic1 = random.choice(pics.keys())
     pic2 = random.choice(pics.keys())
     return render_template("hello.html", pic1 = pic1, pic2 = pic2)
 
-credentials = json.loads(open('tumblr_credentials.json', 'r').read())
-client = pytumblr.TumblrRestClient(credentials["consumer_key"], credentials["consumer_secret"], credentials["oauth_token"], credentials["oauth_token_secret"])
 
-following = client.following()
+def setup():
+    credentials = json.loads(open('tumblr_credentials.json', 'r').read())
 
-#create list of blogs, put in list
-blogs = following["blogs"]
-tosearch = []
-for blog in blogs:
-    tosearch.append(blog["url"])
+    consumer = oauth.Consumer(credentials["consumer_key"], credentials["consumer_secret"])
+    client = oauth.Client(consumer)
+    resp, content = client.request(request_token_url, "POST")
+    request_token = dict(urlparse.parse_qsl(content))
+    print request_token
+    #authorize URL time!
+    print "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
 
-#create dictionary with original picture URLs and set the count to 0
-pics = {}
-for blog in tosearch:
-    resp = client.posts(blog[7:].rstrip('/'), type='photo', tag='me')
-    for post in resp["posts"]:
-        for orig in post["photos"]:
-            pics[orig["original_size"]["url"]] = 0
+    oauth_verifier = raw_input('Insert oauth_verifier')
+    #http://www.tumblr.com/oauth/todo.us?oauth_token=NpIuSQeS7ylqQLE9myDXbo97zHwQCW2lCxp15mw8FvmDjZoJqr&oauth_verifier=fze5EExWh54b9Ocv0FdbHXEos1HrxHriV6bsfp175iCioyClyg#_=_
+    #sign request?
+    token = oauth.Token(request_token['oauth_token'],
+        request_token['oauth_token_secret'])
+    token.set_verifier(oauth_verifier)
+    client = oauth.Client(consumer, token)
 
+    resp, content = client.request(access_token_url, "POST")
+    access_token = dict(urlparse.parse_qsl(content))
+    #get final tokens?
+    print "    - oauth_token        = %s" % access_token['oauth_token']
+    print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
+
+    oauth_token = access_token['oauth_token']
+    oauth_token_secret = access_token['oauth_token_secret']
+
+    client = pytumblr.TumblrRestClient(credentials["consumer_key"], credentials["consumer_secret"], oauth_token, oauth_token_secret)
+
+    following = client.following()
+
+    #create list of blogs, put in list
+    blogs = following["blogs"]
+    tosearch = []
+    for blog in blogs:
+        tosearch.append(blog["url"])
+
+    #create dictionary with original picture URLs and set the count to 0
+    global pics
+    pics = {}
+    for blog in tosearch:
+        resp = client.posts(blog[7:].rstrip('/'), type='photo', tag='me')
+        for post in resp["posts"]:
+            for orig in post["photos"]:
+                pics[orig["original_size"]["url"]] = 0
+    global setupdone
+    setupdone = True
     
     
 #WHY WAS THIS SO HARD    
